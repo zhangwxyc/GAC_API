11d6c22a85a7c926a42d96fd4b3de10a5e55fc54
zhangwstar <zhangwstar@163.com>
2017-05-09 14:34:49 +0800

    1.0

---
+++ ConsoleApplicationTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dd = new TridentResult<object>();
+            if (!Equals(dd.DataResult, null))
+            {
+            }
+
+        }
+        public class TridentResult<T> 
+        {
+            /// <summary>
+            /// 返回结果
+            /// </summary>
+            public T DataResult
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
